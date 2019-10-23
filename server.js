61baa2218d55190c8c6fa01c0c408d33db6faccf
Anthony Dominguez <Anthonydv7@outlook.com>
2019-10-23 12:19:24 -0500

    still trying to deploy

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 app.use(express.static(path.join(__dirname, "../client/build")));
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(routes);
 
